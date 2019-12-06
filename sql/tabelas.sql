7e7c4c426b4eec60ca31e6f913174fde7630e9d8
Pablo Rodrigues <39467624+pfrrodrigues@users.noreply.github.com>
2019-12-06 16:49:46 -0300

    new table.sql

--- sql/tabelas.sql
+++ sql/tabelas.sql
@@ -1,4 +1,3 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE Leitor (
     email VARCHAR(100) PRIMARY KEY,
@@ -7,7 +6,7 @@ CREATE TABLE Leitor (
     sexo CHAR(1),
     nascimento DATE,
     Leitor_TIPO INT,
-    fk_Pais_codPais UUID
+    fk_Pais_codPais SERIAL
 );
 
 CREATE TABLE Livro (
@@ -15,68 +14,68 @@ CREATE TABLE Livro (
     titulo VARCHAR(100),
     descr TEXT,
     nmrPag INTEGER,
-    fk_formato_formato_PK UUID,
+    fk_formato_formato_PK serial,
     anoPublic DATE,
     imagem VARCHAR(100)
 );
 
 CREATE TABLE Evento (
-    codEvento UUID DEFAULT uuid_generate_v4 (),
+    codEvento SERIAL,
     hora TIME,
     data DATE,
     descricao TEXT,
     Evento_TIPO INT,
     fk_Leitor_email VARCHAR(100),
     fk_Leitor_email_ VARCHAR(100),
-    fk_Endereco_codEnd UUID,
+    fk_Endereco_codEnd SERIAL,
     PRIMARY KEY (codEvento)
 );
 
 CREATE TABLE Grupo (
-    codGrupo UUID DEFAULT uuid_generate_v4 (),
+    codGrupo SERIAL,
     nome VARCHAR(100),
     fk_Leitor_email VARCHAR(100),
     PRIMARY KEY (codGrupo)
 );
 
 CREATE TABLE Pais (
-    codPais UUID DEFAULT uuid_generate_v4 (),
+    codPais SERIAL,
     nome VARCHAR(100),
     PRIMARY KEY (codPais)
 );
 
 CREATE TABLE Genero (
-    codGenero UUID DEFAULT uuid_generate_v4 (),
+    codGenero SERIAL,
     nome VARCHAR(100),
     PRIMARY KEY(codGenero)
 );
 
 CREATE TABLE Tag_Grupo (
-    codTagg UUID DEFAULT uuid_generate_v4 (),
+    codTagg SERIAL,
     tagg VARCHAR(100),
     PRIMARY KEY(codTagg)
 );
 
 CREATE TABLE Lista (
-    codLista UUID DEFAULT uuid_generate_v4 (),
+    codLista SERIAL,
     nome VARCHAR(100),
     fk_Leitor_email VARCHAR(100),
     PRIMARY KEY(codLista)
 );
 
 CREATE TABLE Tag_Lista (
-    codTagl UUID DEFAULT uuid_generate_v4 (),
+    codTagl SERIAL,
     tagl VARCHAR(100),
     PRIMARY KEY(codTagl)
 );
 
 CREATE TABLE Mensagem_PARTICIPA (
     fk_Leitor_email VARCHAR(100),
-    fk_Grupo_codGrupo UUID
+    fk_Grupo_codGrupo serial
 );
 
 CREATE TABLE Post (
-    codPost UUID DEFAULT uuid_generate_v4 (),
+    codPost SERIAL,
     PRIMARY KEY (codPost)
 );
 
@@ -85,13 +84,13 @@ CREATE TABLE Endereco (
     rua VARCHAR(100),
     bairro VARCHAR(100),
     nmrEstabelecimento INTEGER,
-    codEnd UUID DEFAULT uuid_generate_v4 (),
-    fk_Pais_codPais UUID,
+    codEnd SERIAL,
+    fk_Pais_codPais serial,
     PRIMARY KEY (codEnd)
 );
 
 CREATE TABLE formato (
-    formato_PK UUID DEFAULT uuid_generate_v4 (),
+    formato_PK SERIAL,
     formato VARCHAR(20),
     PRIMARY KEY(formato_PK)
 );
@@ -115,31 +114,31 @@ CREATE TABLE ESCRITA (
 
 CREATE TABLE PARTICIPACAO (
     FK_Leitor_email VARCHAR(100),
-    FK_Evento_codEvento UUID
+    FK_Evento_codEvento serial
 );
 
 CREATE TABLE POSSE_GENERO (
     FK_Livro_ISBN CHAR(10),
-    FK_Genero_codGenero UUID
+    FK_Genero_codGenero serial
 );
 
 CREATE TABLE CONTEM (
-    FK_Lista_codLista UUID,
+    FK_Lista_codLista serial,
     FK_Livro_ISBN CHAR(10)
 );
 
 CREATE TABLE POSSE_TAGG (
-    FK_Grupo_codGrupo UUID,
-    FK_Tag_Grupo_codTagg UUID
+    FK_Grupo_codGrupo serial,
+    FK_Tag_Grupo_codTagg serial
 );
 
 CREATE TABLE POSSE_TAGL (
-    FK_Lista_codLista UUID,
-    FK_Tag_Lista_codTagl UUID
+    FK_Lista_codLista serial,
+    FK_Tag_Lista_codTagl serial
 );
 
 CREATE TABLE POSTAGEM (
-    FK_Post_codPost UUID
+    FK_Post_codPost serial
 );
 
 ALTER TABLE Leitor ADD CONSTRAINT FK_Leitor_2
