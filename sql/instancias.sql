0f7f83e4eef81ba2ace96d5ccf5785cbacbd8506
Pablo Rodrigues <39467624+pfrrodrigues@users.noreply.github.com>
2019-12-06 23:05:51 -0300

    Add files via upload

---
+++ sql/instancias.sql
@@ -0,0 +1,197 @@
+
+-- Instancias de formato
+INSERT INTO formato(formato) VALUES('Paperback');
+INSERT INTO formato(formato) VALUES('Hardcover');
+INSERT INTO formato(formato) VALUES('Leather Bound');
+INSERT INTO formato(formato) VALUES('Audio CD');
+INSERT INTO formato(formato) VALUES('Paper');
+INSERT INTO formato(formato) VALUES('Library Binding');
+INSERT INTO formato(formato) VALUES('Audiobook');
+INSERT INTO formato(formato) VALUES('Kindle');
+
+-- instances of Livros
+INSERT INTO Livro VALUES('1982110562','The Institute: A Novel', '...', 544, 1, '2019-09-10',
+'http://www.stephenking.com.br/wp-content/uploads/2019/02/theinstitute.jpg?w=640');
+
+INSERT INTO Livro VALUES('125025809X','The Rise of Magicks: Chronicles of The One, Book 3',
+'After the sickness known as the Doom destroyed civilization, magick has become commonplace, and Fallon Swift has spent her young years learning its ways. Fallon cannot live in peace until she frees those who have been preyed upon by the government or the fanatical Purity Warriors, endlessly hunted or locked up in laboratories, brutalized for years on end. She is determined to save even those who have been complicit with this evil out of fear or weakness―if, indeed, they can be saved.',
+464,1, '2019-11-26','https://images-na.ssl-images-amazon.com/images/I/51Xr7lcgmcL._SX329_BO1,204,203,200_.jpg');
+
+INSERT INTO Livro VALUES('038549081X','The Handmaid"s Tale', 'In Margaret Atwood’s dystopian future, environmental disasters and declining birthrates have led to a Second American Civil War. The result is the rise of the Republic of Gilead, a totalitarian regime that enforces rigid social roles and enslaves the few remaining fertile women. Offred is one of these, a Handmaid bound to produce children for one of Gilead’s commanders. Deprived of her husband, her child, her freedom, and even her own name, Offred clings to her memories and her will to survive. At once a scathing satire, an ominous warning, and a tour de force of narrative suspense, The Handmaid’s Tale is a modern classic.',
+311,1, '1998-03-16','https://images-na.ssl-images-amazon.com/images/I/61JEruJjWIL.jpg');
+
+INSERT INTO Livro VALUES('1451673310','Fahrenheit 451', 'Guy Montag is a fireman. His job is to destroy the most illegal of commodities, the printed book, along with the houses in which they are hidden. Montag never questions the destruction and ruin his actions produce, returning each day to his bland life and wife, Mildred, who spends all day with her television “family.” But when he meets an eccentric young neighbor, Clarisse, who introduces him to a past where people didn’t live in fear and to a present where one sees the world through the ideas in books instead of the mindless chatter of television, Montag begins to question everything he has ever known.',
+249, 1, '2012-01-10','https://images-na.ssl-images-amazon.com/images/I/41qI9quGIdL._SX324_BO1,204,203,200_.jpg');
+
+INSERT INTO Livro VALUES('0307744434','The Night Circus', 'The circus arrives without warning. No announcements precede it. It is simply there, when yesterday it was not. Within the black-and-white striped canvas tents is an utterly unique experience full of breathtaking amazements. It is called Le Cirque des Rêves, and it is only open at night...',
+401, 8,'2011-09-13','https://images-na.ssl-images-amazon.com/images/I/81mSNsiLjLL.jpg');
+
+INSERT INTO Livro VALUES('1408855690','Harry Potter and the Order of the Phoenix, Book 5', 'We could tell you, but then we`d have to Obliviate your memory.',
+870, 2, '2003-07-01','https://img1.od-cdn.com/ImageType-100/3450-1/%7B2E490A9B-EDD6-4B56-871A-E6170B532FC8%7DImg100.jpg');
+
+INSERT INTO Livro VALUES('0545010225','Harry Potter and the Deathly Hallows, Book 7', 'As he climbs into the sidecar of Hagrids motorbike and takes to the skies, leaving Privet Drive for the last time, Harry Potter knows that Lord Voldemort and the Death Eaters are not far behind. The protective charm that has kept Harry safe until now is broken, but he cannot keep hiding. The Dark Lord is breathing fear into everything Harry loves and to stop him Harry will have to find and destroy the remaining Horcruxes. The final battle must begin - Harry must stand and face his enemy....',
+784,2, '2007-07-01','https://img1.od-cdn.com/ImageType-100/3450-1/%7BE4610191-F47E-4B1A-9461-54F930F8EFF1%7DImg100.jpg');
+
+INSERT INTO Livro VALUES('0375508325','Cosmos', 'Cosmos is one of the bestselling science books of all time. In clear-eyed prose, Sagan reveals a jewel-like blue world inhabited by a life form that is just beginning to discover its own identity and to venture into the vast ocean of space. Featuring a new Introduction by Sagan’s collaborator, Ann Druyan, full color illustrations, and a new Foreword by astrophysicist Neil deGrasse Tyson, Cosmos retraces the fourteen billion years of cosmic evolution that have transformed matter into consciousness, exploring such topics as the origin of life, the human brain, Egyptian hieroglyphics, spacecraft missions, the death of the Sun, the evolution of galaxies, and the forces and individuals who helped to shape modern science.
+', 432, 8, '2013-12-07','https://images-na.ssl-images-amazon.com/images/I/51SX54fcT-L._SX286_BO1,204,203,200_.jpg');
+
+INSERT INTO Livro VALUES('7536693966','The Dark Forest (Remembrance of Earths Past Book 2)', 'In The Dark Forest, Earth is reeling from the revelation of a coming alien invasion-in just four centuries time. The aliens human collaborators may have been defeated, but the presence of the sophons, the subatomic particles that allow Trisolaris instant access to all human information, means that Earths defense plans are totally exposed to the enemy. Only the human mind remains a secret. This is the motivation for the Wallfacer Project, a daring plan that grants four men enormous resources to design secret strategies, hidden through deceit and misdirection from Earth and Trisolaris alike. Three of the Wallfacers are influential statesmen and scientists, but the fourth is a total unknown. Luo Ji, an unambitious Chinese astronomer and sociologist, is baffled by his new status. All he knows is that hes the one Wallfacer that Trisolaris wants dead.',
+513, 8, '2015-10-11','https://images-na.ssl-images-amazon.com/images/I/51jYXmPtC2L._SX329_BO1,204,203,200_.jpg');
+
+
+-- Instancias de Pais
+INSERT INTO Pais(nome) VALUES ('Albania');
+INSERT INTO Pais(nome) VALUES ('American Samoa');
+INSERT INTO Pais(nome) VALUES ('Anguilla');
+INSERT INTO Pais(nome) VALUES ('Antigua and Barbuda');
+INSERT INTO Pais(nome) VALUES ('Armenia');
+INSERT INTO Pais(nome) VALUES ('Aruba');
+INSERT INTO Pais(nome) VALUES ('Australia');
+INSERT INTO Pais(nome) VALUES ('Bahamas');
+INSERT INTO Pais(nome) VALUES ('Bahrain');
+INSERT INTO Pais(nome) VALUES ('Barbados');
+INSERT INTO Pais(nome) VALUES ('Belize');
+INSERT INTO Pais(nome) VALUES ('Bermuda');
+INSERT INTO Pais(nome) VALUES ('Bosnia and Herzegovina');
+INSERT INTO Pais(nome) VALUES ('Botswana');
+INSERT INTO Pais(nome) VALUES ('Brazil');
+INSERT INTO Pais(nome) VALUES ('British Virgin Islands');
+INSERT INTO Pais(nome) VALUES ('Cabo Verde');
+INSERT INTO Pais(nome) VALUES ('Canada');
+INSERT INTO Pais(nome) VALUES ('Cayman Islands');
+INSERT INTO Pais(nome) VALUES ('Chile');
+INSERT INTO Pais(nome) VALUES ('China');
+INSERT INTO Pais(nome) VALUES ('Hong Kong');
+INSERT INTO Pais(nome) VALUES ('Macao');
+INSERT INTO Pais(nome) VALUES ('Colombia');
+INSERT INTO Pais(nome) VALUES ('Cook Islands');
+INSERT INTO Pais(nome) VALUES ('Costa Rica');
+INSERT INTO Pais(nome) VALUES ('Curaçao');
+INSERT INTO Pais(nome) VALUES ('Dominica');
+INSERT INTO Pais(nome) VALUES ('Faroe Islands');
+INSERT INTO Pais(nome) VALUES ('Fiji');
+INSERT INTO Pais(nome) VALUES ('Former Yugoslav Republic of Macedonia');
+INSERT INTO Pais(nome) VALUES ('Georgia');
+INSERT INTO Pais(nome) VALUES ('Greenland');
+INSERT INTO Pais(nome) VALUES ('Grenada');
+INSERT INTO Pais(nome) VALUES ('Guam');
+INSERT INTO Pais(nome) VALUES ('Guernsey');
+INSERT INTO Pais(nome) VALUES ('Iceland');
+INSERT INTO Pais(nome) VALUES ('India');
+INSERT INTO Pais(nome) VALUES ('Indonesia');
+INSERT INTO Pais(nome) VALUES ('Isle of Man');
+INSERT INTO Pais(nome) VALUES ('Israel');
+INSERT INTO Pais(nome) VALUES ('Jamaica');
+INSERT INTO Pais(nome) VALUES ('Japan');
+INSERT INTO Pais(nome) VALUES ('Jersey');
+INSERT INTO Pais(nome) VALUES ('Jordan');
+INSERT INTO Pais(nome) VALUES ('Korea');
+INSERT INTO Pais(nome) VALUES ('Malaysia');
+INSERT INTO Pais(nome) VALUES ('Maldives');
+INSERT INTO Pais(nome) VALUES ('Mauritius');
+INSERT INTO Pais(nome) VALUES ('Mongolia');
+INSERT INTO Pais(nome) VALUES ('Montenegro');
+INSERT INTO Pais(nome) VALUES ('Montserrat');
+INSERT INTO Pais(nome) VALUES ('Morocco');
+INSERT INTO Pais(nome) VALUES ('Namibia');
+INSERT INTO Pais(nome) VALUES ('New Caledonia');
+INSERT INTO Pais(nome) VALUES ('Norway');
+INSERT INTO Pais(nome) VALUES ('Oman');
+INSERT INTO Pais(nome) VALUES ('Panama');
+INSERT INTO Pais(nome) VALUES ('Peru');
+INSERT INTO Pais(nome) VALUES ('Qatar');
+INSERT INTO Pais(nome) VALUES ('Saint Kitts and Nevis');
+INSERT INTO Pais(nome) VALUES ('Saint Lucia');
+INSERT INTO Pais(nome) VALUES ('Saint Vincent and the Grenadines');
+INSERT INTO Pais(nome) VALUES ('Samoa');
+INSERT INTO Pais(nome) VALUES ('Saudi Arabia');
+INSERT INTO Pais(nome) VALUES ('Serbia');
+INSERT INTO Pais(nome) VALUES ('Seychelles');
+INSERT INTO Pais(nome) VALUES ('Singapore');
+INSERT INTO Pais(nome) VALUES ('South Africa');
+INSERT INTO Pais(nome) VALUES ('Swaziland');
+INSERT INTO Pais(nome) VALUES ('Taiwan');
+INSERT INTO Pais(nome) VALUES ('Thailand');
+INSERT INTO Pais(nome) VALUES ('Trinidad and Tobago');
+INSERT INTO Pais(nome) VALUES ('Tunisia');
+INSERT INTO Pais(nome) VALUES ('Turkey');
+INSERT INTO Pais(nome) VALUES ('Turks and Caicos Islands');
+INSERT INTO Pais(nome) VALUES ('United Arab Emirates');
+INSERT INTO Pais(nome) VALUES ('United Kingdom');
+INSERT INTO Pais(nome) VALUES ('United States');
+INSERT INTO Pais(nome) VALUES ('Uruguay');
+INSERT INTO Pais(nome) VALUES ('US Virgin Islands');
+INSERT INTO Pais(nome) VALUES ('Viet Nam');
+INSERT INTO Pais(nome) VALUES ('Andorra');
+INSERT INTO Pais(nome) VALUES ('Liechtenstein');
+INSERT INTO Pais(nome) VALUES ('Monaco');
+INSERT INTO Pais(nome) VALUES ('San Marino');
+INSERT INTO Pais(nome) VALUES ('Switzerland');
+INSERT INTO Pais(nome) VALUES ('Marshall Islands');
+INSERT INTO Pais(nome) VALUES ('Nauru');
+INSERT INTO Pais(nome) VALUES ('Niue');
+INSERT INTO Pais(nome) VALUES ('Palau');
+INSERT INTO Pais(nome) VALUES ('Vanuatu');
+
+-- Instancias de Leitor
+INSERT INTO Leitor VALUES('stephenking@email.com', 'Stephen King','StephenTheKing', 'M', '1947-09-21',0, 79);
+INSERT INTO Leitor VALUES('noraroberts@email.com', 'Nora Roberts','noraroberts', 'F', '1950-10-10', 0, 79);
+INSERT INTO Leitor VALUES('margaretat@email.com', 'Margaret Atwood','margofmargot?', 'F', '1939-11-18', 0, 19);
+INSERT INTO Leitor VALUES('ray@email.com','Ray Bradbury','isitastrongpasswd?','M', '1920-07-22',0,79);
+INSERT INTO Leitor VALUES('erinm@email.com', 'Erin Morgenstern', 'whoami?', 'F', '1978-07-08', 0, 79);
+INSERT INTO Leitor VALUES('jk@email.com', 'J. K. Rowling', 'wingardiumLeviosa', 'F', '1965-07-31', 0, 93);
+INSERT INTO Leitor VALUES('cs@email.com', 'Carl Sagan', 'iloveanndruyan', 'M', '1934-11-09', 0, 79);
+INSERT INTO Leitor VALUES('cixin@email.com', 'Cixin Liu', 'dongfangyanxu', 'M', '1963-06-23', 0, 22);
+
+-- Instancias de genero
+INSERT INTO Genero(nome) VALUES('Fantasy');
+INSERT INTO Genero(nome) VALUES('Romance');
+INSERT INTO Genero(nome) VALUES('Science Fiction');
+INSERT INTO Genero(nome) VALUES('Thriller');
+INSERT INTO Genero(nome) VALUES('Classic');
+INSERT INTO Genero(nome) VALUES('Historical Fiction');
+INSERT INTO Genero(nome) VALUES('Horror');
+INSERT INTO Genero(nome) VALUES('Mystery');
+INSERT INTO Genero(nome) VALUES('Biography');
+INSERT INTO Genero(nome) VALUES('Journalism');
+INSERT INTO Genero(nome) VALUES('Poetry');
+INSERT INTO Genero(nome) VALUES('Autobiography');
+INSERT INTO Genero(nome) VALUES('Humour');
+INSERT INTO Genero(nome) VALUES('Young Adult Fiction');
+INSERT INTO Genero(nome) VALUES('Dictionary');
+INSERT INTO Genero(nome) VALUES('Textbook');
+INSERT INTO Genero(nome) VALUES('Science and Math');
+INSERT INTO Genero(nome) VALUES('Self-Help');
+INSERT INTO Genero(nome) VALUES('Religion and Spirituality');
+INSERT INTO Genero(nome) VALUES('Computers & Technology');
+
+-- Instancias de ESCRITA
+INSERT INTO ESCRITA VALUES('stephenking@email.com','1982110562');
+INSERT INTO ESCRITA VALUES('noraroberts@email.com','125025809X');
+INSERT INTO ESCRITA VALUES('margaretat@email.com','038549081X');
+INSERT INTO ESCRITA VALUES('ray@email.com','1451673310');
+INSERT INTO ESCRITA VALUES('erinm@email.com','0307744434');
+INSERT INTO ESCRITA VALUES('jk@email.com','1408855690');
+INSERT INTO ESCRITA VALUES('jk@email.com','0545010225');
+INSERT INTO ESCRITA VALUES('cs@email.com','0375508325');
+INSERT INTO ESCRITA VALUES('cixin@email.com','7536693966');
+
+-- Instancias de POSSE_GENERO
+INSERT INTO POSSE_GENERO VALUES('1982110562', 4);
+INSERT INTO POSSE_GENERO VALUES('1982110562', 7);
+INSERT INTO POSSE_GENERO VALUES('1982110562', 8);
+INSERT INTO POSSE_GENERO VALUES('125025809X', 1);
+INSERT INTO POSSE_GENERO VALUES('125025809X', 2);
+INSERT INTO POSSE_GENERO VALUES('125025809X', 3);
+INSERT INTO POSSE_GENERO VALUES('125025809X', 4);
+INSERT INTO POSSE_GENERO VALUES('038549081X', 3);
+INSERT INTO POSSE_GENERO VALUES('1451673310', 3);
+INSERT INTO POSSE_GENERO VALUES('1451673310', 6);
+INSERT INTO POSSE_GENERO VALUES('0307744434', 1);
+INSERT INTO POSSE_GENERO VALUES('1408855690', 1);
+INSERT INTO POSSE_GENERO VALUES('1408855690', 14);
+INSERT INTO POSSE_GENERO VALUES('0545010225', 1);
+INSERT INTO POSSE_GENERO VALUES('0545010225', 14);
+INSERT INTO POSSE_GENERO VALUES('0375508325', 17);
+INSERT INTO POSSE_GENERO VALUES('7536693966', 3);
+INSERT INTO POSSE_GENERO VALUES('7536693966', 6);
