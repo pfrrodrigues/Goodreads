26d81bac31e5880265fe8228c02991574c242dd3
ArthurCamargo <arthurcamargo13@gmail.com>
2019-12-15 02:03:58 -0300

    Adiciona novas consultadas nos sqls e permite deletar listas

--- src/lists.py
+++ src/lists.py
@@ -32,8 +32,7 @@ def select_list(lista, cursor):
                                    join leitor e on (escrita.fk_leitor_email=e.email)
                                    where livro.isbn = %s;
                                    """, (book[0],))
-            book = cursor.fetchall()
-            book = book[0]
+            book = cursor.fetchone()
 
             if book:
                 t.add_row([str(counter_books), book[1], book[3]])
@@ -106,71 +105,68 @@ def search_by_name(num_itens, cursor):
         else:
             print("Commando inválido" + new_line)
 
-#search_by_tag(10, cur)
-
-def show_your_lists(num_itens, current_user, conn, cursor):
-    """ Show the lists that you follow """
-
+def search_by_tag(num_itens, cursor):
+    """ Procura uma lista por suas tags """
     os.system('clear')
+    new_line = "\n"
+    msg = "Digite o nome da tag que deseja procurar:" + new_line + ">>"
+    lista_nome = input(msg)
+
+
+    t = PrettyTable(['Opção', 'Nome', 'Tags', 'Livros'])#cabecario da tabela
+
+    cursor.execute("""SELECT lista.codlista, lista.nome FROM lista
+                      JOIN posse_tagl ON (lista.codlista = posse_tagl.fk_lista_codlista)
+                      JOIN tag_lista ON (posse_tagl.fk_tag_lista_codtagl = tag_lista.codtagl)
+                      WHERE tag_lista.tagl LIKE %s""", (lista_nome + '%',))
+    lists = cursor.fetchall()
+
+    counter_lists = 1
+    for lista in lists:
+        cursor.execute("""SELECT tagl FROM lista
+                          JOIN posse_tagl ON (lista.codlista = posse_tagl.fk_lista_codlista)
+                          JOIN tag_lista ON (posse_tagl.fk_tag_lista_codtagl = tag_lista.codtagl)
+                          WHERE lista.codlista = %s""", (lista[0],))
+        your_tagls = cursor.fetchall()
+
+        cursor.execute("""SELECT livro.titulo FROM lista
+                          JOIN contem ON (lista.codlista = contem.fk_lista_codlista)
+                          JOIN livro ON (contem.fk_livro_isbn= livro.isbn)
+                          WHERE lista.codlista = %s """, (lista[0],))
+        livros = cursor.fetchall()
+
+        pretty_books = ''#Printa os livros mais bonitos
+        for livro in livros:
+            pretty_books += str(livro[0]) + new_line
+
+        pretty_tags = '' #printa mais bonitos a tag
+        for tag in your_tagls:
+
+            pretty_tags += '#'
+            pretty_tags += str(tag[0]) + ', '#adiciona o newline com a tag
+
+        t.add_row([str(counter_lists), lista[1], str(pretty_tags), str(pretty_books)])
+        counter_lists += 1
+
     logged = True
-    new_line = '\n'
 
-    while(logged):
-        counter_lists = 1
-        cursor.execute("""SELECT lista.nome FROM lista
-                        WHERE fk_leitor_email = %s""", (current_user[0],))
-        your_lists = cursor.fetchall()
-
-        t = PrettyTable(['Opção','Nome', 'Tags', 'Livros'])#cabecario da tabela
-
-        for lista in your_lists:
-            cursor.execute("""SELECT tagl FROM lista
-                              JOIN posse_tagl ON (lista.codlista = posse_tagl.fk_lista_codlista)
-                              JOIN tag_lista ON (posse_tagl.fk_tag_lista_codtagl = tag_lista.codtagl)
-                              WHERE fk_leitor_email = %s AND lista.nome = %s
-                              """, (current_user[0], lista,))
-            your_tagls = cursor.fetchall()
-
-            cursor.execute("""SELECT livro.titulo FROM lista
-                              JOIN contem ON (lista.codlista = contem.fk_lista_codlista)
-                              JOIN livro ON (contem.fk_livro_isbn= livro.isbn)
-                              WHERE fk_leitor_email = %s AND lista.nome = %s""", (current_user[0],lista,))
-            livros = cursor.fetchall()
-
-            pretty_books = ''#Printa os livros mais bonitos
-            for livro in livros:
-                pretty_books += str(livro[0]) + new_line
-
-            pretty_tags = '' #Printa mais bonitos a tag
-            for tag in your_tagls:
-
-                pretty_tags += '#'
-                pretty_tags += str(tag[0]) + ', '#adiciona o newline com a tag
-
-            t.add_row([str(counter_lists), lista[0], str(pretty_tags), str(pretty_books)])
-            counter_lists += 1
+    while logged:
 
         print(t) #Printa lista
 
-        msg = "Voce gostaria de:" + new_line
-        new = "[1] Criar uma nova lista" + new_line
-        edit = "[2] Editar uma lista" + new_line
-        quit = "[3] Voltar" + new_line + '>>'
+        msg = "Para ver os livros de alguma lista pressione a sua Opção" + new_line
+        quit ="[q] Voltar" + new_line + ">>"
+        msg += quit
 
-        msg += new + edit + quit
-
-        section = input(msg)
-
-        if section == '1':
-            create_new_list(current_user, conn, cursor)
-        elif section == '2':
-            delete_list()
-        else:
+        opt = input(msg)
+        if opt == 'q':
             logged = False
-        os.system('clear')
-
-def delete_list():
-    """ Pagina para deleter uma lista """
+        elif opt.isdecimal():
+            opt = int(opt)
+            if opt > 0 and opt < counter_lists:
+                select_list(lists[opt-1][0], cursor)
+        else:
+            print("Commando inválido" + new_line)
 
 def pesquisar_livros(livros, cursor):
     """Pagina para pesquisar livros a fim de adicionar em uma lista """
@@ -233,7 +229,6 @@ def create_new_list(current_user, conn, cursor):
     new_line = '\n'
 
     while(logged):
-        new_line = "\n"
         #Nome da lista
         msg = "Qual sera o nome da nova lista:" + new_line + ">>"
         lnome = input(msg)
@@ -288,10 +283,214 @@ def create_new_list(current_user, conn, cursor):
                     print("Commando incorreto..." + new_line)
                     input()
 
+def choose_lists(lists):
+    """ Pagina para escolher um numero de lista"""
+
+    logged = True
+    new_line = '\n'
+
+    msg = "Selecione a lista que deseja editar:" + new_line
+    quit ="[q] Voltar" + new_line + ">>"
+    msg += quit
+
+    while(logged):
+        opt = input(msg)
+        if opt == 'q':
+            logged = False
+        elif opt.isdecimal():
+            opt = int(opt)
+            if opt > 0 and opt <= len(lists):
+                return lists[opt - 1][1]
+        else:
+            print("Commando inválido" + new_line)
+
+def delete_lists(lists, conn, cursor):
+    """ Deleta uma dada lista """
+
+    cursor.execute("""DELETE FROM posse_tagl
+                      WHERE fk_lista_codlista = %s""", (lists,))
+
+    cursor.execute("""DELETE FROM contem
+                      WHERE fk_lista_codlista = %s""", (lists,))
+
+    cursor.execute("""DELETE FROM lista
+                      WHERE lista.codlista = %s""", (lists,))
+
+    cursor.execute("""DELETE FROM tag_lista
+                      WHERE NOT EXISTS(SELECT * FROM posse_tagl
+                                       WHERE fk_tag_lista_codtagl = codtagl)""")
+    conn.commit()
+    print("Lista deletada com sucesso ...")
+    input()
+
+def alter_lists(lists, conn, cursor):
+    """ Altera uma dada lista """
+
+    logged = True
+    new_line = '\n'
+
+    msg = "Voce deseja:" + new_line
+    nome = "[1] Alterar o nome da lista" + new_line
+    tags = "[2] Alterar as tags da lista" + new_line
+    livros = "[3] Alterar os livros da lista" + new_line
+    quit ="[q] Voltar" + new_line + ">>"
+
+    msg += nome
+    msg += tags
+    msg += livros
+    msg += quit
 
 
-def show_friend_lists(num_itens, cursor):
-    """ Show the lists that your friends follow """
+    while(logged):
+        print_lista(lists, cursor)
+        opt = input(msg)
+        if opt == 'q':
+            logged = False
+        elif opt == '1':
+            pass
+            #alter_nome(lists)
+        elif opt == '2':
+            pass
+            #alter_tags(lists)
+        elif opt == '3':
+            pass
+            #alter_livros(lists)
+        else:
+            print("Commando inválido" + new_line)
+    pass
+
+def print_lista(lists, cursor):
+    """ imprime uma lista dado seu codlista """
+
+    new_line = "\n"
+
+    t = PrettyTable(['Opção', 'Nome', 'Tags', 'Livros'])#cabecario da tabela
+
+    cursor.execute("""SELECT lista.codlista, lista.nome FROM lista
+                      WHERE lista.codlista = %s""", (lists,))
+    lists = cursor.fetchall()
+
+    counter_lists = 1
+    for lista in lists:
+        cursor.execute("""SELECT tagl FROM lista
+                          JOIN posse_tagl ON (lista.codlista = posse_tagl.fk_lista_codlista)
+                          JOIN tag_lista ON (posse_tagl.fk_tag_lista_codtagl = tag_lista.codtagl)
+                          WHERE lista.codlista = %s""", (lista[0],))
+        your_tagls = cursor.fetchall()
+
+        cursor.execute("""SELECT livro.titulo FROM lista
+                          JOIN contem ON (lista.codlista = contem.fk_lista_codlista)
+                          JOIN livro ON (contem.fk_livro_isbn= livro.isbn)
+                          WHERE lista.codlista = %s """, (lista[0],))
+        livros = cursor.fetchall()
+
+        pretty_books = ''#Printa os livros mais bonitos
+        for livro in livros:
+            pretty_books += str(livro[0]) + new_line
+
+        pretty_tags = '' #printa mais bonitos a tag
+        for tag in your_tagls:
+
+            pretty_tags += '#'
+            pretty_tags += str(tag[0]) + ', '#adiciona o newline com a tag
+
+        t.add_row([str(counter_lists), lista[1], str(pretty_tags), str(pretty_books)])
+        counter_lists += 1
+
+
+        print(t) #Printa lista
+
+def edit_lists_menu(lists, conn, cursor):
+    """ Menu de edicao de lista """
+
+    logged = True
+    new_line = '\n'
+
+    msg = "Voce deseja:" + new_line
+    delete = "[1] Deletar essa lista" + new_line
+    modify = "[2] Modificar essa lista" + new_line
+    quit ="[q] Voltar" + new_line + ">>"
+
+    msg += delete
+    msg += modify
+    msg += quit
+
+    while(logged):
+
+        print_lista(lists, cursor)
+
+        opt = input(msg)
+        if opt == 'q':
+            logged = False
+        elif opt == '1':
+            delete_lists(lists, conn, cursor)
+            logged = False
+        elif opt == '2':
+            alter_lists(lists, conn, cursor)
+        else:
+            print("Commando inválido" + new_line)
+
+def show_your_lists(num_itens, current_user, conn, cursor):
+    """ Show the lists that you follow """
+
+    os.system('clear')
+    logged = True
+    new_line = '\n'
+
+    while(logged):
+        counter_lists = 1
+        cursor.execute("""SELECT lista.nome, lista.codlista FROM lista
+                        WHERE fk_leitor_email = %s""", (current_user[0],))
+        your_lists = cursor.fetchall()
+
+        t = PrettyTable(['Opção','Nome', 'Tags', 'Livros'])#cabecario da tabela
+
+        for lista in your_lists:
+            cursor.execute("""SELECT tagl FROM lista
+                              JOIN posse_tagl ON (lista.codlista = posse_tagl.fk_lista_codlista)
+                              JOIN tag_lista ON (posse_tagl.fk_tag_lista_codtagl = tag_lista.codtagl)
+                              WHERE fk_leitor_email = %s AND lista.nome = %s
+                              """, (current_user[0], lista[0],))
+            your_tagls = cursor.fetchall()
+
+            cursor.execute("""SELECT livro.titulo FROM lista
+                              JOIN contem ON (lista.codlista = contem.fk_lista_codlista)
+                              JOIN livro ON (contem.fk_livro_isbn= livro.isbn)
+                              WHERE fk_leitor_email = %s AND lista.nome = %s""", (current_user[0],lista[0],))
+            livros = cursor.fetchall()
+
+            pretty_books = ''#Printa os livros mais bonitos
+            for livro in livros:
+                pretty_books += str(livro[0]) + new_line
+
+            pretty_tags = '' #Printa mais bonitos a tag
+            for tag in your_tagls:
+
+                pretty_tags += '#'
+                pretty_tags += str(tag[0]) + ', '#adiciona o newline com a tag
+
+            t.add_row([str(counter_lists), lista[0], str(pretty_tags), str(pretty_books)])
+            counter_lists += 1
+
+        print(t) #Printa lista
+
+        msg = "Voce gostaria de:" + new_line
+        new = "[1] Criar uma nova lista" + new_line
+        edit = "[2] Editar uma lista" + new_line
+        quit = "[3] Voltar" + new_line + '>>'
+
+        msg += new + edit + quit
+
+        section = input(msg)
+
+        if section == '1':
+            create_new_list(current_user, conn, cursor)
+        elif section == '2':
+            codlista = choose_lists(your_lists)
+            edit_lists_menu(codlista, conn, cursor)
+        else:
+            logged = False
+        os.system('clear')
 
 def lists_page(current_user):
     """ Implements page of lists """
@@ -309,13 +508,11 @@ def lists_page(current_user):
         name_lists = '[1] Procurar uma lista pelo nome' + newline
         tag_lists = '[2] Procurar uma lista pela tag' + newline
         your_lists = '[3] Suas listas' + newline
-        friend_lists = '[4] Listas dos seus amigos' + newline
-        quit = '[5] Voltar ao menu inicial' + newline + ">>"
+        quit = '[4] Voltar ao menu inicial' + newline + ">>"
 
         msg += name_lists
         msg += tag_lists
         msg += your_lists
-        msg += friend_lists
         msg += quit
 
         section = input(msg)
@@ -325,12 +522,9 @@ def lists_page(current_user):
         if section == '1':
             search_by_name(10, cur)
         elif section == '2':
-            pass
-            #search_by_tag(10, cur)
+            search_by_tag(10, cur)
         elif section == '3':
             show_your_lists(10, current_user, conn, cur)
-            pass
-            #show_friend_lists(10, current_user, cur)
         else:
             logged = False
         os.system('clear')
